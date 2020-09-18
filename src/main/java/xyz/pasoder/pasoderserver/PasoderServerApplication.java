9968dbb8991ff9b026a3a617bcf518480060ea13
haickey <853415914@qq.com>
2020-09-18 19:21:07 +0800

    项目初始化

---
+++ src/main/java/xyz/pasoder/pasoderserver/PasoderServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.pasoder.pasoderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasoderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasoderServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/pasoder/pasoderserver/PasoderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pasoder.pasoderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasoderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
