bea04af732b6373551a8d06ca280cd77173be208
haickey <853415914@qq.com>
2020-10-01 15:05:57 +0800

    启动引导调整

--- src/main/java/xyz/pasoder/pasoderserver/PasoderServerApplication.java
+++ src/main/java/xyz/pasoder/pasoderserver/PasoderServerApplication.java
@@ -1,15 +1,14 @@
 package xyz.pasoder.pasoderserver;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "xyz.pasoder.pasoderserver")
-@MapperScan(basePackages = {"xyz.pasoder.pasoderserver.password.store.dao"})
+@MapperScan(basePackages = {"xyz.pasoder.pasoderserver.**.dao"})
 public class PasoderServerApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(PasoderServerApplication.class, args);
     }
 
