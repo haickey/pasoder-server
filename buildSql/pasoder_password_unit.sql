a674da1ab0569d3e69b261883b06dda71d95f89b
haickey <853415914@qq.com>
2020-10-01 12:44:29 +0800

    创建索引&可阅读性修饰

--- buildSql/pasoder_password_unit.sql
+++ buildSql/pasoder_password_unit.sql
@@ -1,13 +1,14 @@
 create table password_unit
 (
     password_unit_id bigint auto_increment primary key,
-    ip               varchar(15)                                                              not null default '0.0.0.0',
-    name             varchar(50)                                                              not null,
-    password         varchar(800)                                                             not null,
-    description      varchar(120),
-    create_time      timestamp(6) default CURRENT_TIMESTAMP(6)                                null,
-    update_time      timestamp(6) default CURRENT_TIMESTAMP(6) on update CURRENT_TIMESTAMP(6) null
+    ip               varchar(15)  not null default '0.0.0.0',
+    name             varchar(50)  not null,
+    password         varchar(800) not null,
+    description      varchar(120) null,
+    create_time      timestamp(6) null     default CURRENT_TIMESTAMP(6),
+    update_time      timestamp(6) null     default CURRENT_TIMESTAMP(6) on update CURRENT_TIMESTAMP(6)
 );
+create index unq_password_unit on password_unit (ip, name);
 
 
 
