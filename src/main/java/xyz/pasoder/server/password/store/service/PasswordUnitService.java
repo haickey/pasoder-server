91300845b7c73af4f6e77b8e310276f1f8a3d95a
haickey <853415914@qq.com>
2020-10-13 19:59:30 +0800

    添加dozer

---
+++ src/main/java/xyz/pasoder/server/password/store/service/PasswordUnitService.java
@@ -0,0 +1,23 @@
+package xyz.pasoder.server.password.store.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import xyz.pasoder.server.password.store.dao.PasswordUtilDao;
+import xyz.pasoder.server.password.store.pojo.PasswordUnit;
+import xyz.pasoder.server.password.store.pojo.PasswordUnitDO;
+
+import java.util.List;
+
+@Service
+public class PasswordUnitService {
+
+    @Autowired
+    PasswordUtilDao passwordUtilDao;
+
+    public List<PasswordUnit> findPasswordUnitByIp(final String ip) {
+        final List<PasswordUnitDO> passwords = passwordUtilDao.findPasswordsByIp(ip);
+        return null;
+    }
+
+
+}
